6c03f89c33aa42e9d6179d703653d18c4d4bc5d8
Neculae Andrei-Fabian <andrei.fabian188@gmail.com>
2023-04-02 05:51:24 +0300

    Updated Classes

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -61,8 +61,8 @@ str Game::get_input()
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
-    newt.c_cc[VMIN] = 0;
-    newt.c_cc[VTIME] = 1;
+    newt.c_cc[VMIN]{0};
+    newt.c_cc[VTIME]{1};
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     while (elapsed_time < 1000)
     {
